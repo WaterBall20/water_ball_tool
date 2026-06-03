5cfe4caf31570c3f0026df83a5197a2b217c4a85
开发水球 <shuiqiu20@outlook.com>
2026-06-03 17:06:15 +0800

    备份

--- src/gakumasu/data.rs
+++ src/gakumasu/data.rs
@@ -2,72 +2,69 @@ use std::sync::Arc;
 
 //流派类型
 enum CardType {
-    Emotion,//感性
-    Logic,//理性
-    Esper,//非凡
-    Neutral,//通用
+    Emotion, //感性
+    Logic, //理性
+    Esper, //非凡
+    Neutral, //通用
 }
 //指针类型
 enum PointerStateType {
-    None,//没有
-    Some,//存在
-    Strong(u8),//强气
-    FullPower,//全力
-    WarmUp(u8)//温存
+    None, //没有
+    Some, //存在
+    Strong(u8), //强气
+    FullPower, //全力
+    WarmUp(u8), //温存
 }
 //效果类型
 enum EffectType {
-    Stamina,//体力
-    Vigour,//元气
-    Concentration,//集中
-    Tuning,//好调
-    Imperial,//好印象
-    Motivation,//干劲
-    FullPowerValue,//全力值
-    FullPower,//全力
-    Pointer(PointerStateType),//指针
-    Turn,//回合
+    //数值
+    NumberValue {
+        value: u8,
+        count: u8,
+    },
+    Stamina(i8), //体力
+    //元气
+    Vigour {
+        value: i16,
+        sum_all: bool,
+    },
+    Concentration(i16), //集中
+    Tuning(i16), //好调
+    Imperial(i16), //好印象
+    Motivation(u16), //干劲
+    FullPowerValue(i8), //全力值
+    Pointer(PointerStateType), //指针
+    Turn(i8), //回合
 }
 //消费类型
 enum ConsumptionType {
     //体力
     Stamina {
         value: u8,
-        no_vigour: bool//不使用元气，体力消费
+        no_vigour: bool, //不使用元气，体力消费
     },
-    Concentration(u8),//集中
-    Tuning(u8),//好调,
-    Impression(u8),//好印象
-    Motivation(u8),//干劲
-    FullPowerValue(u8),//全力值
-}
-//卡牌效果类型
-enum CardEffectType {
-    //数值
-    NumberValue {
-        value: u8,
-        count: u8,
-    },
-
+    Concentration(u8), //集中
+    Tuning(u8), //好调,
+    Impression(u8), //好印象
+    Motivation(u8), //干劲
+    FullPowerValue(u8), //全力值
 }
 
 //效果条件类型
-enum EffectIfType {
-}
+enum EffectIfType {}
 //卡牌效果
 struct CardEffect {
-    card_effect_type: CardEffectType,
+    card_effect_type: EffectType,
     effect_if_type: EffectIfType,
 }
 //运行时效果
-enum CardRunningEffect {
-}
+enum CardRunningEffect {}
 
 //技能卡
 struct SkillCard {
     name: String,
-    consumption_type: ConsumptionType,//消费类型
-    card_effects: Vec<CardEffect>,//卡牌效果
+    consumption_type: ConsumptionType, //消费类型
+    card_effects: Vec<CardEffect>, //卡牌效果
     use_count: Option<u8>, //使用次数
 }
 
@@ -86,5 +83,5 @@ struct FieldEffectCard {
 //场地效果卡运行时
 struct FieldEffectCardRunning {
     id: String,
-    field_effect_card: Arc<FieldEffectCard>
+    field_effect_card: Arc<FieldEffectCard>,
 }
