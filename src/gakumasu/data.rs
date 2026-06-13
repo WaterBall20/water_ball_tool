c5bea5cc5ad7befa0f632cb47b4af3af6e3bc3f2
WaterBall20 <shuiqiu20@outlook.com>
2026-06-13 22:01:44 +0800

    BF

--- src/gakumasu/data.rs
+++ src/gakumasu/data.rs
@@ -1,62 +1,92 @@
+//数据结构体和枚举
 use std::sync::Arc;
 
-//流派类型
+/*
+* 关于数字类型的说明
+* 为简化计算转换，只要参与最终“数值”的计算，均为f32
+*   其余如次数，可使用usize，用于循环
+*/
+
+//卡牌类型
 enum CardType {
-    Sense, //感性
-    Logic, //理性
-    Anomaly, //非凡
-    Neutral, //通用
+    Skill(Box<CardType>), //技能卡
+    ActiveSkill,          //
+    MentalSkill,
 }
+
 //指针类型
-enum PointerStateType {
-    None, //没有
-    Some, //存在
-    Strong(u8), //强气
-    FullPower, //全力
-    WarmUp(u8), //温存
+enum Cuideline {
+    Confident(f32),      //强气
+    FullPower,           //全力
+    FullPowerPoint(f32), //全力值
+    Preservation(f32),   //温存
+}
+
+//属性类型
+enum Attribute {
+    //=== 角色属性 ===
+    Vocal, //歌唱
+    Visual,
+    Dance, //舞蹈
+    //参数
+    Parameter { value: f32, count: usize },
+    Stamina(f32),                                      //体力
+    StaminaConsumptionCut { value: f32 },              //消费体力削减
+    StaminaConsumptionReduction { turn_count: usize }, //消费体力减少
+    AdditionalStaminaConsumption { value: f32 },       //消费体力追加
+    IncreasedStaminaConsumption { turn_count: usize }, //消费体力追加
+    Energy(f32),                                       //元气
+    Concentration(f32),                                //集中
+    Tuning(f32),                                       //好调
+    Imperial(f32),                                     //好印象
+    Motivation(f32),                                   //干劲
+    Guideline(Option<Cuideline>),                      //指针
+    NegateDecreaseState(usize),                        //低下状态无效
+    FixedGuideline(usize),                             //指针固定
+    PoorCondition { turn_count: usize },               //不调
+    Anxiety { turn_count: usize },                     //不安 TODO:需要检查效果
+    Timid { turn_count: usize },                       //弱气 TODO:需要验证效果
 }
 //效果类型
-enum EffectType {
-    //数值
-    NumberValue {
-        value: u8,
-        count: u8,
+enum Effect {
+    //属性变更
+    Attribute(Attribute),
+    //含条件的效果
+    Conditions {
+        r#if: EffectConditions,
+        effect_type: Box<EffectConditions>,
     },
-    Stamina(i8), //体力
-    //元气
-    Vigour {
-        value: i16,
-        sum_all: bool,
-    },
-    Concentration(i16), //集中
-    Tuning(i16), //好调
-    Imperial(i16), //好印象
-    Motivation(u16), //干劲
-    FullPowerValue(i8), //全力值
-    Pointer(PointerStateType), //指针
-    Turn(i8), //回合
 }
 //消费类型
 enum ConsumptionType {
     //体力
     Stamina {
-        value: u8,
-        no_vigour: bool, //不使用元气，体力消费
+        value: f32,
+        is_consumption: bool, //是否体力消费
     },
-    Concentration(u8), //集中
-    Tuning(u8), //好调,
-    Impression(u8), //好印象
-    Motivation(u8), //干劲
-    FullPowerValue(u8), //全力值
+    Concentration(f32),  //集中
+    Tuning(f32),         //好调,
+    Impression(f32),     //好印象
+    Motivation(f32),     //干劲
+    FullPowerValue(f32), //全力值
 }
 
 //效果条件类型
-enum EffectIfType {}
-//卡牌效果
-struct CardEffect {
-    card_effect_type: EffectType,
-    effect_if_type: EffectIfType,
+enum EffectConditions {
+    //=== 比较类型 ===
+    //偶像技能卡卡牌
+    IdolSkillCard {
+        count: usize,                //次数
+        card: Arc<SkillCardRunning>, //技能卡
+    },
 }
+
+//触发类型
+enum TriggerTiming {
+    TurnStart,//回合开始
+    TurnEnd,//回合结束
+}
+
 //运行时效果
 enum CardRunningEffect {}
 
@@ -64,8 +94,7 @@ enum CardRunningEffect {}
 struct SkillCard {
     name: String,
     consumption_type: ConsumptionType, //消费类型
-    card_effects: Vec<CardEffect>, //卡牌效果
-    use_count: Option<u8>, //使用次数
+    effects: Vec<Effect>,              //卡牌效果
 }
 
 //技能卡运行时
--- src/gakumasu/simulator.rs
+++ src/gakumasu/simulator.rs
@@ -1,3 +1,12 @@
 struct GameSimulator {
     
 }
+
+//游戏运行时
+struct GameRunning {
+    dance: f32,
+    visual: f32,
+    vocal : f32,
+    turn_count: usize,
+    turn_index: usize,
+}
